b8a5af5a031e83fd75300fe1039caab0029d3b35
benjaminlanthier <lanthier_benji@hotmail.com>
2024-11-27 15:31:27 -0500

    Translate `str` to `bool` array

--- observables.py
+++ observables.py
@@ -6,7 +6,7 @@ class PauliObservable:
     """
     Class permettant de représenter une observable de Pauli.
     """
-    def __init__(self, p: str):
+    def __init__(self, obs: str):
         """
         Description
         -----------
@@ -14,7 +14,31 @@ class PauliObservable:
 
         Paramètre
         ---------
-        p: str (?)
+        obs: str
             Une observable de Pauli.
         """
-        pass
+        self.obs = obs
+        self.str_to_bool(obs)
+
+    def str_to_bool(self, obs: str):
+        """
+        Traduire les observables en valeurs booléennes pour la représentation en tableau.
+
+        Paramètre
+        ---------
+        obs: str
+            L'observable à traduire sous la forme d'un vecteur booléen.
+        """
+        # Initialize the tableau with all False
+        num_qubits = len(obs)
+        self.obs_bool = np.zeros(2*num_qubits, dtype=bool)
+
+        # Parse the observable
+        for idx, str_obs in enumerate(self.obs):
+            if str_obs == 'x':  # X operator
+                self.obs_bool[idx] = True
+            elif str_obs == 'z':  # Z operator
+                self.obs_bool[idx+num_qubits] = True
+            elif str_obs == 'y': # Y operator
+                self.obs_bool[idx] = True
+                self.obs_bool[idx+num_qubits] = True
