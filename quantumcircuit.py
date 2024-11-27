2e3b5d3cb665fff5f82d72cc08cb07425a6f99de
benjaminlanthier <lanthier_benji@hotmail.com>
2024-11-27 15:32:14 -0500

    Build the `QuantumCircuit` class

--- quantumcircuit.py
+++ quantumcircuit.py
@@ -1,4 +1,7 @@
+import json
 import numpy as np
+
+from observables import PauliObservable
 
 
 
@@ -19,4 +22,26 @@ class QuantumCircuit:
             liste avec les valeurs propres pour lesquelles calculer les probabilités de mesure, et
             des dictionnaires avec les portes à appliquer.
         """
-        pass
+        self.l = l
+        for circ in self.l:
+            circ[1] = PauliObservable(circ[1])
+            # self._translate_eig_vals_to_bool(circ[2])
+
+    # def _translate_eig_vals_to_bool(self, eig_vals: list):
+    #     """
+    #     Traduire les valeurs propres en valeurs booléennes pour une représentation plus légères des
+    #     informations.
+    #     """
+    #     zeros = np.zeros(len(eig_vals), dtype=bool)
+    #     for eig_val_idx, eig_val in enumerate(eig_vals):
+    #         if eig_val == "-":
+    #             zeros[eig_val_idx] = True
+    #     circ[2] = zeros
+
+
+if __name__ == "__main__":
+    with open('dummy_circuits.json', 'r') as file:
+        l = json.load(file)
+    print(l)
+    q = QuantumCircuit(l)
+    print(q.l)
