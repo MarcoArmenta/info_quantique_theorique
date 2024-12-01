20bc0c20366054766776837e472733bc871d585c
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-01 15:36:24 -0500

    simulator.py

---
+++ simulator.py
@@ -0,0 +1,9 @@
+from quantumcircuit import QuantumCircuit
+from observables import PauliObservable
+
+class GKSimulator:
+    def __init__(self):
+        pass
+
+    def measure_pauli(self, circuit, observable):
+        pass
