f33f8607b20176cdc6799def736d76ddc84746d6
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-01 17:37:48 -0500

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,9 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: lenovo
+"""
+
+
 from quantumcircuit import QuantumCircuit
-from observables import PauliObservable
+from multiprocessing import Pool
 
 class GKSimulator:
-    def __init__(self):
+    def __init__(self, quantum_circuit):
+        self.quantum_circuit = quantum_circuit
+
+    def measure_pauli(self, observable):
         pass
 
-    def measure_pauli(self, circuit, observable):
-        pass
+    def run(self, circuits):
+        with Pool(processes=8) as pool:
+            results = pool.map(self.simulate_single_circuit, circuits)
+        return results
+
+    
+    def simulate_single_circuit(self, circuit):
+        return self.measure_pauli(circuit)
