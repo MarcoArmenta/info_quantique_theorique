85d360d74d5d19b639718a739fb02736ad974012
zringra <ydmazri@gmail.com>
2024-12-03 23:11:27 -0500

    observables.py

---
+++ observables.py
@@ -0,0 +1,22 @@
+class PauliObservable:
+    def __init__(self, chaine_pauli):
+        self.chaine_pauli = chaine_pauli
+        self.n_qubits = len(chaine_pauli)
+
+    def convertir_en_px_pz(self): # Comme on extrait nos observable du fichier json, on les réecrit sous forme vectorielle
+        P_x = [0] * self.n_qubits
+        P_z = [0] * self.n_qubits
+        for i, pauli in enumerate(self.chaine_pauli):
+            if pauli == 'X':
+                P_x[i] = 1
+            elif pauli == 'Z':
+                P_z[i] = 1
+            elif pauli == 'Y':
+                P_x[i] = 1
+                P_z[i] = 1
+        return {'P_x': P_x, 'P_z': P_z}
+
+    def appliquer(self, circuit): # on applique l'observable de pauli au circuit donné.
+        for q, pauli in enumerate(self.chaine_pauli):
+            if pauli != 'I':  # Ignore 'I' (identité)
+                circuit.application_de_pauli(q, pauli)
