294fb64edb2e8feef274a70a680bb4957f45532d
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-05 01:34:36 -0500

    Update observables.py

--- observables.py
+++ observables.py
@@ -1,1 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 14:42:28 2024
 
+@author: lenovo
+"""
+
+class PauliObservable:
+    ''' Initialise une observable de Pauli'''
+    def __init__(self,pauli_chaine):
+   
+   
+        self.pauli_chaine=pauli_chaine
+        
+    ''' Mesure l'observable en fonction des stabilisateurs'''
+#cette partie est généré par IA
+    def mesurer(self, stabilisateurs):
+        
+        return {"+":0.5, "-":0.5}
