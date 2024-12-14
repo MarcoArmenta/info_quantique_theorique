3440043afaf53f6a1fa86998fdfc72d07429a63b
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-13 22:44:46 -0500

    observables.py

--- observables.py
+++ observables.py
@@ -4,16 +4,9 @@ Created on Wed Dec  4 14:42:28 2024
 
 @author: lenovo
 """
-
 class PauliObservable:
-    ''' Initialise une observable de Pauli'''
-    def __init__(self,pauli_chaine):
-   
-   
-        self.pauli_chaine=pauli_chaine
-        
-    ''' Mesure l'observable en fonction des stabilisateurs'''
-#cette partie est généré par IA
+    def __init__(self, pauli_chaine):
+        self.pauli_chaine = pauli_chaine
+
     def mesurer(self, stabilisateurs):
-        
-        return {"+":0.5, "-":0.5}
+        return {"+": 0.5, "-": 0.5}
