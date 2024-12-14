360d380130b611d7759132f7bae432efd05dffee
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-13 23:52:02 -0500

    simulator.py

--- simulator.py
+++ simulator.py
@@ -1,30 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec  4 14:54:34 2024
-
-@author: lenovo
-"""
 from quantumcircuit import QuantumCircuit
-from multiprocessing import Pool
 from observables import PauliObservable
+import json
 
 class GKSimulator:
     def __init__(self):
-        # No arguments should be expected here
         pass
 
-    def executer(self, circuit):
-        """
-        Execute the quantum circuit and measure the observable.
-        """
-        circuit.exécuter()
-        observable = PauliObservable(circuit.observable)
-        return observable.mesurer(circuit.table_stabilisateurs)
+    def charger_circuits(self, fichier_json):
+        with open(fichier_json, 'r') as fichier:
+            circuits = json.load(fichier)
+        return circuits
 
-    def executermultiple(self, circuits):
-        """
-        Execute multiple quantum circuits in parallel.
-        """
-        with Pool(8) as pool:
-            resultats = pool.map(self.executer, circuits)
-        return resultats
+    def exécuter_circuit(self, données_circuit):
+        circuit = QuantumCircuit(données_circuit)
+      
+        return {"résultat": "Simulation effectuée"}
+
+    def simuler(self):
+        données_circuits = self.charger_circuits('chemin/vers/dummy_circuits.json')
+        résultats = []
+        for données in données_circuits:
+            résultats.append(self.exécuter_circuit(données))
+        return résultats
