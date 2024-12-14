a49e01170d27710676597ada07afe23ea99fbc2b
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-13 22:43:05 -0500

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -4,35 +4,27 @@ Created on Wed Dec  4 14:54:34 2024
 
 @author: lenovo
 """
-
-from multiprocessing import Pool
 from quantumcircuit import QuantumCircuit
+from multiprocessing import Pool
 from observables import PauliObservable
 
-
-
 class GKSimulator:
-    
-    ''' cette partie initialise le simulateur sans dépendre d'un circuit spécifique'''
-       
     def __init__(self):
-        
-   
+        # No arguments should be expected here
         pass
-    ''' Cette méthode executer exécute les portes du circuit  '''
-    def executer(self,circuit):
-        
-        circuit.executer()
-        observable=PauliObservable(circuit.observable)
-        return observable.mesurer(circuit.stabilisateurs)
-    
-    
-    ''' cette partie simule plusieurs circuits en parallèle'''
 
-    def executermultiple(self,circuits):
-       
-   #cette partie est généré par IA
-        
+    def executer(self, circuit):
+        """
+        Execute the quantum circuit and measure the observable.
+        """
+        circuit.exécuter()
+        observable = PauliObservable(circuit.observable)
+        return observable.mesurer(circuit.table_stabilisateurs)
+
+    def executermultiple(self, circuits):
+        """
+        Execute multiple quantum circuits in parallel.
+        """
         with Pool(8) as pool:
             resultats = pool.map(self.executer, circuits)
         return resultats
