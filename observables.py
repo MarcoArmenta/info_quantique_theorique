b3b48eac05be82c6b364ca68ba03b170d39c8a81
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-13 23:53:27 -0500

    observables.py

--- observables.py
+++ observables.py
@@ -1,12 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec  4 14:42:28 2024
-
-@author: lenovo
-"""
 class PauliObservable:
-    def __init__(self, pauli_chaine):
-        self.pauli_chaine = pauli_chaine
+    def __init__(self, chaine_pauli):
+        self.chaine_pauli = chaine_pauli
+        self.n_qubits = len(chaine_pauli)
 
-    def mesurer(self, stabilisateurs):
-        return {"+": 0.5, "-": 0.5}
+    def convertir_en_px_pz(self):
+        P_x = [0] * self.n_qubits
+        P_z = [0] * self.n_qubits
+        for i, pauli in enumerate(self.chaine_pauli):
+            if pauli == 'X':
+                P_x[i] = 1
+            elif pauli == 'Z':
+                P_z[i] = 1
+            elif pauli == 'Y':
+                P_x[i], P_z[i] = 1, 1
+        return {'P_x': P_x, 'P_z': P_z}
+
+    def appliquer_observable(self, circuit):
+        for q, pauli in enumerate(self.chaine_pauli):
+            if pauli != 'I':
+                circuit.appliquer_pauli(q, pauli)
