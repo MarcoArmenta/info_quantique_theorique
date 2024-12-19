e7cdbd7ac615a7abfa95bdf89be549ee0a71b8de
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-18 21:31:31 -0500

    observables.py

--- observables.py
+++ observables.py
@@ -1,27 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Créé le Mer 4 Déc 2024 à 14:40:00
+Created on Wed Dec  4 14:42:28 2024
 
 @author: lenovo
 """
 class PauliObservable:
-    def __init__(self, chaine_pauli):
-        self.chaine_pauli = chaine_pauli
-        self.n_qubits = len(chaine_pauli)
+    def __init__(self, observable):
+        
+        self.observable = observable
 
-    def convertir_en_px_pz(self):
-        P_x = [0] * self.n_qubits
-        P_z = [0] * self.n_qubits
-        for i, pauli in enumerate(self.chaine_pauli):
-            if pauli == 'X':
-                P_x[i] = 1
-            elif pauli == 'Z':
-                P_z[i] = 1
-            elif pauli == 'Y':
-                P_x[i], P_z[i] = 1, 1
-        return {'P_x': P_x, 'P_z': P_z}
-
-    def appliquer_observable(self, circuit):
-        for q, pauli in enumerate(self.chaine_pauli):
-            if pauli != 'I':
-                circuit.appliquer_pauli(q, pauli)
+    def measure(self, stabilizer_table):
+      
+        probabilities = {"+1": 0.5, "-1": 0.5}  
+        return probabilities
