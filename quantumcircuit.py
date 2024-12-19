c3ac3abe2766ecc36d61602d67b54cc968bad6a4
Madjid1999 <abdelmadjid.hamma@usherbrooke.ca>
2024-12-18 21:29:20 -0500

    quantumcircuit.py

--- quantumcircuit.py
+++ quantumcircuit.py
@@ -5,37 +5,76 @@ Créé le Mer 4 Déc 2024 à 14:40:00
 @author: lenovo
 """
 import numpy as np
+
 class QuantumCircuit:
-    def __init__(self, données_circuit):
-        self.n_qubits = données_circuit[0]
-        self.stabilisateurs = []
-        for i in range(self.n_qubits):
-            V_x = [0] * self.n_qubits  
-            V_z = [0] * self.n_qubits 
-            V_z[i] = 1  
-            self.stabilisateurs.append({'V_x': V_x, 'V_z': V_z})
+    def __init__(self, circuits):
+       
+        self.circuits = []
+        for circuit_description in circuits:
+            circuit = {
+                "num_qubits": circuit_description[0],
+                "observable": circuit_description[1],
+                "eigenvalues": circuit_description[2],
+                "gates": circuit_description[3:]
+            }
+            circuit["stabilizer_table"] = self._initialize_stabilizer_table(circuit["num_qubits"])
+            self.circuits.append(circuit)
 
-    def appliquer_hadamard(self, q):
-        for stabilisateur in self.stabilisateurs:
-            valeur_temporaire = stabilisateur['V_x'][q]
-            stabilisateur['V_x'][q] = stabilisateur['V_z'][q]
-            stabilisateur['V_z'][q] = valeur_temporaire
+    def _initialize_stabilizer_table(self, num_qubits):
+       
+        stabilizer_table = []
+        for i in range(num_qubits):
+            z = [1 if j == i else 0 for j in range(num_qubits)]
+            x = [0] * num_qubits
+            stabilizer_table.append((z, x))
+        return stabilizer_table
 
-    def appliquer_phase_S(self, q):
-        for stabilisateur in self.stabilisateurs:
-            stabilisateur['V_z'][q] = (stabilisateur['V_z'][q] + stabilisateur['V_x'][q]) % 2
+    def apply_gate(self, circuit, gate, targets):
+       
+        if gate == "h":
+            self._apply_hadamard(circuit, targets[0])
+        elif gate == "s":
+            self._apply_s_gate(circuit, targets[0])
+        elif gate == "x":
+            self._apply_pauli_x(circuit, targets[0])
+        elif gate == "z":
+            self._apply_pauli_z(circuit, targets[0])
+        elif gate == "y":
+            self._apply_pauli_y(circuit, targets[0])
+        elif gate == "cx":
+            self._apply_cx_gate(circuit, targets[0], targets[1])
+        else:
+            raise ValueError(f"Porte inconnue : {gate}")
 
-    def appliquer_CNOT(self, cible, controle):
-        for stabilisateur in self.stabilisateurs:
-            stabilisateur['V_z'][controle] = (stabilisateur['V_z'][controle] + stabilisateur['V_z'][cible]) % 2
-            stabilisateur['V_x'][cible] = (stabilisateur['V_x'][cible] + stabilisateur['V_x'][controle]) % 2
+    def _apply_hadamard(self, circuit, qubit):
+       
+        for z, x in circuit["stabilizer_table"]:
+            z[qubit], x[qubit] = x[qubit], z[qubit]
 
-    def appliquer_pauli(self, q, pauli):
-        for stabilisateur in self.stabilisateurs:
-            if pauli == 'X':
-                stabilisateur['V_x'][q], stabilisateur['V_z'][q] = stabilisateur['V_z'][q], stabilisateur['V_x'][q]
-            elif pauli == 'Z':
-                stabilisateur['V_x'][q] = (stabilisateur['V_x'][q] + stabilisateur['V_z'][q]) % 2
-            elif pauli == 'Y':
-                stabilisateur['V_x'][q] = (stabilisateur['V_x'][q] + stabilisateur['V_z'][q]) % 2
-                stabilisateur['V_z'][q] = (stabilisateur['V_z'][q] + stabilisateur['V_x'][q]) % 2
+    def _apply_s_gate(self, circuit, qubit):
+        
+        for z, x in circuit["stabilizer_table"]:
+            if x[qubit] == 1:
+                z[qubit] ^= x[qubit]
+
+    def _apply_pauli_x(self, circuit, qubit):
+       
+        for z, x in circuit["stabilizer_table"]:
+            x[qubit] ^= 1
+
+    def _apply_pauli_z(self, circuit, qubit):
+        
+        for z, x in circuit["stabilizer_table"]:
+            z[qubit] ^= 1
+
+    def _apply_pauli_y(self, circuit, qubit):
+        
+        for z, x in circuit["stabilizer_table"]:
+            z[qubit] ^= 1
+            x[qubit] ^= 1
+
+    def _apply_cx_gate(self, circuit, control, target):
+        
+        for z, x in circuit["stabilizer_table"]:
+            z[target] ^= z[control]
+            x[control] ^= x[target]
